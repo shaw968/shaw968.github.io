5e7f357ffd8d5ec5a49a96a2a2c40973d9f0d41e
shaw968 <46874089+shaw968@users.noreply.github.com>
2019-01-28 13:39:18 -0800

    Update test.js

--- homework1/test.js
+++ homework1/test.js
@@ -1,4 +1,13 @@
+/*Stephen Shaw
+  sshaw7@my.smccd.edu
+  CIS 128
+  homework 1
+  test.js
+  January 25, 2019
+*/
 
-	    $(document).ready(function() {
-		    $("#pic").fadeTo(3000,.3).fadeTo(2000,1);
-		});
+"use strict";
+
+$(document).ready(function() {
+    $("#pic").fadeTo(3000,.3).fadeTo(2000,1);
+});
