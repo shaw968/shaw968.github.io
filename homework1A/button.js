f774af101b62429adefae8db143581ec92f6f6d1
shaw968 <46874089+shaw968@users.noreply.github.com>
2019-02-08 16:07:47 -0800

    Add files via upload

---
+++ homework1A/button.js
@@ -0,0 +1,22 @@
+/*Stephen Shaw
+  sshaw7@my.smccd.edu
+  CIS 128
+  homework 1
+  button.js
+  January 31, 2019
+*/
+
+/*"use strict";*/
+$(document).ready(function() {
+	$("#button1").click(function() {
+		$("h1").animate( 
+		{ fontSize: "38px" }, 2000)
+		.animate( 
+		{ fontSize: "28px" }, 3000, function(){
+	        $("#pic").animate(
+		      { borderWidth: "2px" }, "fast");
+		      $("h1").css("color", "rgba(106, 90, 205,.7)");
+		    });  
+	});
+ });
+ 
