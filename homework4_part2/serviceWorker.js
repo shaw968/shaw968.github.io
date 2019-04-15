7251d9d7a6116c71d0b95a3993e7ec797efaff2b
shaw968 <46874089+shaw968@users.noreply.github.com>
2019-04-14 17:47:40 -0700

    Add files via upload

---
+++ homework4_part2/serviceWorker.js
@@ -0,0 +1,23 @@
+/*  Stephen Shaw
+    sshaw7@my.smccd.edu
+	CIS 128
+	serviceWorker.js
+	Homework 4 part 2
+	04/13/2019
+*/
+var cacheVersion = "ver-1"
+var cachedFiles = ["index.html", "style.css", "lightblue.jpg", "lightgold.jpg", "downArrow.png"]
+
+
+self.oninstall = function(event) {
+	console.log("installed");
+	caches.open(cacheVersion);
+	cache.addAll(cachedFiles);
+}
+
+self.onactivate = function(event) {
+	console.log("activated");
+}
+
+self.onfetch = function(event) {}
+
