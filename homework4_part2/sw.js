03445934ee2c73cce1974edbe1f4783b99e13600
shaw968 <46874089+shaw968@users.noreply.github.com>
2019-04-17 15:35:16 -0700

    Add files via upload

--- homework4_part2/sw.js
+++ homework4_part2/sw.js
@@ -1,20 +1,19 @@
 /*  Stephen Shaw
     sshaw7@my.smccd.edu
-    CIS 128
-    sw.js
-    Homework 4 part 2
-    04/13/2019
+	CIS 128
+	sw.js
+	Homework 4 part 2
+	04/13/2019
 */
 var cacheVersion = "ver-1";
-var cachedFiles = ["index.html", "style.css", 
-		   "images/lightblue.jpg", "images/lightgold.jpg", "images/downArrow.png", "images/margarita.png"];
+var cachedFiles = ["index.html", "style.css", "manifest.json", "images/lightblue.jpg", "images/lightgold.jpg", "images/downArrow.png", "images/margarita.png"];
 
 
 self.oninstall = function(event) {
 	console.log("installed");
 	caches.open(cacheVersion).then(function(cache) {
 	    cache.addAll(cachedFiles);
-})
+}
 
 self.onactivate = function(event) {
 	console.log("activated");
